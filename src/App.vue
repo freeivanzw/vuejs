a90240a75c96ffba501904d514f61a5096a76564
freeivanzw <freeivanzw@gmail.com>
2021-11-16 23:29:04 +0200

    d

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<ul>
+		<listOne
+			v-for="listItem in myListData"
+			v-bind:listItem="listItem"
+			v-bind:key="listItem"
+		/>
+	</ul>
+</template>
+
+<script>
+import listOne from "@/components/list-one";
+
+export default {
+	data() {
+		return {
+			myListData: ["1", "2"
+				// {name:'ivan'},
+				// {name:'ruslan'}
+			]
+		}
+	},
+	name: 'App',
+	components: {
+		listOne
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Arial, sans-serif;
+
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/list-one.vue
@@ -0,0 +1,13 @@
+<template>
+    <li>{{listItem}}</li>
+</template>
+
+<script>
+export default {
+    props: {
+        listItem: this
+
+    }
+}
+
+</script>
