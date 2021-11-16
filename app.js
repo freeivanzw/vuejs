a3efd7ae8e4c2bbc2f9210ed2f61e8c9600da09a
freeivanzw <freeivanzw@gmail.com>
2021-11-16 20:41:23 +0200

    documentation vuejs

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
 Vue.createApp({
     data() {
         return {
-            title: 'список чогось там',
-            placeholdertext: 'введіть пункт списку',
-            inputValue: '',
-            listTasks: [],
+            counter: 0,
+            title: 'hello vue.js!'
         }
     },
     methods: {
-        newListItem() {
-            if (this.inputValue != '') {
-                this.listTasks.push(this.inputValue);
-                this.inputValue = ''
-            }
-        },
-        toUp(text) {
-            return text.toUpperCase();
-        },
-        delThisItem(idx) {
-            this.listTasks.splice(idx, 1)
-        },
+        reversMessage() {
+            this.title = this.title.split("").reverse().join("");
+                
+        }
         
-    },
-    computed: {
-        doubleList() {
-            return this.listTasks.length * 2 
-        }
-    },
-    watch: {
-        inputValue(value) {
-            console.log(value)
-        }
     }
 }).mount('#app')
